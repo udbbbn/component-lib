8d571724d47bd0cb70ab6c2e6001c00cf76cca6f
udbbbn <udbbbn@163.com>
2020-03-09 11:51:44 +0800

    feat: 初步的弹幕模块

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Bullet from './bullet'
+
 function App() {
   return (
     <div className="App">
+      <Bullet></Bullet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
