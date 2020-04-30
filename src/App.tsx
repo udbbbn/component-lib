5e296dbb095156b3b9c2744471c101c1dae8007d
udbbbn <udbbbn@163.com>
2020-04-30 16:11:21 +0800

    refactor: 修改空闲轨道逻辑

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Bullet></Bullet>
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -21,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
