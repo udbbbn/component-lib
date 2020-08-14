e49752139a7d773f247dcf2ad482d96b13eae7b4
udbbbn <udbbbn@163.com>
2020-08-14 15:02:55 +0800

    feat: 虚拟列表 新增组件可配置项

--- src/virtual-list/index.tsx
+++ src/virtual-list/index.tsx
@@ -5,13 +5,31 @@ interface VirtualListState {
   list: any[]
   visibleList: any[]
   viewHeight: number
-  listTransY: number
+  listTransY: number // 
 }
 
-const LIST_HEIGHT = 400;
-const ITEM_HEIGHT = 30;
+interface ListSource {
+  key: string | number
+  [x: string]: any
+}
 
-export default class VirtualList extends React.Component<{}, VirtualListState> {
+interface VirtualListProps {
+  itemHeight: number // 根据实际项目情况 预估单个 item 的高度
+  listHeight: number // list 容器高度
+  bufferSize: number // 预渲染 item 数量 建议为双数可均匀分配至当前可视窗口的上下位
+  listSource: ListSource[] // 数据源
+  sourceValueKey: string // 渲染 listSource时 key 的取值
+}
+
+export default class VirtualList extends React.Component<VirtualListProps, VirtualListState> {
+
+  static defaultProps: VirtualListProps = {
+    itemHeight: 45,
+    listHeight: 400,
+    bufferSize: 6,
+    listSource: Array(100).fill(1).map((el, i) => ({ key: i, value: i })),
+    sourceValueKey: 'value'
+  }
 
   listView = React.createRef<HTMLDivElement>()
   startIdx = 0
@@ -21,7 +39,7 @@ export default class VirtualList extends React.Component<{}, VirtualListState> {
     list: [],
     visibleList: [],
     viewHeight: 0,
-    listTransY: 0
+    listTransY: 0,
   }
 
   componentDidMount() {
@@ -29,25 +47,57 @@ export default class VirtualList extends React.Component<{}, VirtualListState> {
   }
 
   initList() {
-    let { list } = this.state;
-    Array(10000).fill(1).map((el, i) => { list.push({ key: i, value: i }) });
+    const { listSource } = this.props;
     this.setState({
-      list,
-      viewHeight: this.getViewHeight(list.length)
+      list: listSource,
+      viewHeight: this.getViewHeight(listSource.length)
     }, () => this.updateVisibleList())
     this.listView.current!.addEventListener('scroll', this.scroll);
   }
 
+  // 更新视图
   updateVisibleList(scrollTop: number = 0) {
-    const visibleCount = Math.ceil(LIST_HEIGHT / ITEM_HEIGHT);
-    this.startIdx = Math.floor(scrollTop / ITEM_HEIGHT);
+    const { itemHeight, listHeight, bufferSize } = this.props;
+    const { list } = this.state;
+    const visibleCount = Math.ceil(listHeight / itemHeight);
+    this.startIdx = Math.floor(scrollTop / itemHeight);
     this.endIdx = this.startIdx + visibleCount;
-    const visibleList = this.state.list.slice(this.startIdx, this.endIdx);
-    const listTransY = this.startIdx * ITEM_HEIGHT;
+    let offsetStart = 0, offsetEnd = 0;
+    // 处理预渲染 
+    if (this.startIdx === 0) {
+      offsetEnd = bufferSize;
+    } else if (this.endIdx === (list.length - 1)) {
+      // 索引从 0 开始 0 - 99 
+      // 因 slice(start end) 中 end 不包含 slice 中 所以 endIdx = 99 是不包含 99 这个值的 所以 offsetStart 要 + 1
+      // offsetEnd 为 1 
+      offsetStart = -bufferSize + 1;
+      offsetEnd = 1;
+      } else {
+      const offsetMiddle = Math.floor(bufferSize / 2);
+      if (this.startIdx > offsetMiddle && list.length >= this.endIdx + offsetMiddle) {
+        offsetStart = -offsetMiddle;
+        offsetEnd = bufferSize - offsetMiddle;
+      } else if (this.startIdx <= offsetMiddle) {
+        offsetStart = -this.startIdx;
+        offsetEnd = bufferSize + offsetStart;
+      } else {
+        // (list.length <= this.endIdx + offsetMiddle)
+        offsetEnd = list.length - this.endIdx;
+        offsetStart = -(bufferSize - offsetEnd);
+      } 
+    }
+    const visibleList = this.state.list.slice(this.startIdx + offsetStart, this.endIdx + offsetEnd)
+    // NOTE 这里需要注意的点是 translate3d 中的 y 值 偏移量也要参与计算 将预渲染的元素不显示在可视窗口内
+    // 如 startIdx 为 10 那就是以 10 计算位移量 而实际渲染时会 unshift(形如) bufferSize 至 10 的前面
+    const listTransY = (this.startIdx + offsetStart) * itemHeight;
     this.setState({ visibleList, listTransY });
   }
 
-  getViewHeight = (listLength: number) => listLength * ITEM_HEIGHT
+  // 获取容器高度
+  getViewHeight(listLength: number) {
+    const { itemHeight } = this.props;
+    return listLength * itemHeight
+  }
 
   scroll = (e: Event) => {
     const { scrollTop } = (e.target as HTMLDivElement);
@@ -55,6 +105,7 @@ export default class VirtualList extends React.Component<{}, VirtualListState> {
   }
 
   render() {
+    const { sourceValueKey, itemHeight } = this.props;
     const { visibleList, viewHeight, listTransY } = this.state;
     return (
       <div className={style.list_view} ref={this.listView}>
@@ -62,7 +113,7 @@ export default class VirtualList extends React.Component<{}, VirtualListState> {
         <div className={style.list_content} style={{ transform: `translate3d(0, ${listTransY}px, 0)` }}>
           {
             visibleList.map(el => (
-              <div className={style.list_item} key={el.key}>{el.value}</div>
+              <div className={style.list_item} style={{ height: `${itemHeight}px` }} key={el.key}>{el[sourceValueKey]}</div>
             ))
           }
         </div>
