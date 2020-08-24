625c2411ddb03930cdc4aa61c32e201fa389a341
udbbbn <udbbbn@163.com>
2020-08-24 23:54:57 +0800

    feat: 下拉刷新新增了三种状态

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,11 +8,28 @@ import VirtualList from './virtual-list'
 
 function App() {
 
+  const [refreshStatus, setRefreshStatus] = useState(0);
+
+  function virtualListRefresh() {
+    setRefreshStatus(1);
+    console.log('refresh');
+    setTimeout(() => {
+      setRefreshStatus(2);
+      setTimeout(() => {
+        setRefreshStatus(0);
+      }, 500)
+    }, 1500);
+  }
+
+  useEffect(() => {
+    console.log('useEffect', refreshStatus);
+  }, [refreshStatus])
+
   return (
     <div className="App">
       {/* <Bullet></Bullet>
       <Particle></Particle> */}
-      <VirtualList></VirtualList>
+      <VirtualList refreshCallBack={virtualListRefresh} refreshStatus={refreshStatus}></VirtualList>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
